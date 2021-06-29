2a5890615837104e43c458fa80d8adfa3eb6d1ee
carmenfu006 <guess_006@hotmail.com>
2021-06-30 00:48:33 +0800

    Edit and update board on show page

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -30,6 +30,24 @@ class BoardsController < ApplicationController
     @board = Board.find(params[:id])
   end
 
+  def edit
+    @board = Board.find(params[:id])
+  end
+
+  def update
+    board = Board.find(params[:id])
+    
+    if board.update(board_params)
+      redirect_to board_path(board)
+    end
+  end
+
+  def destroy
+    board = Board.find(params[:id])
+    board.destroy
+    redirect_to boards_path
+  end
+
   private
     def board_params
       params.require(:board).permit(:name)
--- app/javascript/controllers/board_controller.js
+++ app/javascript/controllers/board_controller.js
@@ -15,14 +15,14 @@ export default class extends Controller {
       let url = response.url
       let form = event.detail.formSubmission.formElement
       let board_name = form.querySelector('[id="board_name"]').value
-      // let board_id = url.split('=')[1]
+      let board_id = url.split('=')[1]
       console.log()
       let element = document.createElement('div');
       let list = this.listTarget
 
       element.innerHTML = `<br>
                           <div class="single-board">
-                            <a href="${url}">
+                            <a href="/boards/${board_id}">
                               ${board_name}
                             </a>  
                           </div>`
