84a0093b4cb2f1cb9669dac6f113daedcb703bdf
carmenfu006 <guess_006@hotmail.com>
2021-06-30 09:48:13 +0800

    Add stimulus data confirm for method delete

--- app/javascript/controllers/confirmation_controller.js
+++ app/javascript/controllers/confirmation_controller.js
@@ -3,14 +3,12 @@ import { Controller } from 'stimulus'
 export default class extends Controller {
   static values = { message: String };
 
-  connect() {
-    console.log(this.messageValue)
-  }
-
   confirm(event) {
     if (!(window.confirm(this.messageValue))) {
       event.preventDefault();
-      event.stopImmediatePropagation();
+
+      // As if you have more than one event handler from being executed, you can add this method here
+      // event.stopImmediatePropagation();
     };
   };
 }
